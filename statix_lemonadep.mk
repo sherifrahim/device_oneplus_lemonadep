dbc0349bc5d0337cdaf27ef75400ad0614b772e1
Vaisakh Murali <mvaisakh@statixos.com>
2022-08-01 16:02:57 +0000

    lemonadep: Init StatiX
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

---
+++ statix_lemonadep.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from lemonadep device
+$(call inherit-product, device/oneplus/lemonadep/device.mk)
+
+# Inherit some common StatiX stuff.
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
+
+PRODUCT_NAME := statix_lemonadep
+PRODUCT_DEVICE := lemonadep
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := LE2125
+
+PRODUCT_SYSTEM_NAME := OnePlus9Pro
+PRODUCT_SYSTEM_DEVICE := OnePlus9Pro
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="OnePlus9Pro-user 12 SKQ1.210216.001 R.202204081955 release-keys" \
+    TARGET_DEVICE=OnePlus9Pro \
+    TARGET_PRODUCT=OnePlus9Pro
+
+BUILD_FINGERPRINT := OnePlus/OnePlus9Pro/OnePlus9Pro:12/SKQ1.210216.001/R.202204081955:user/release-keys
