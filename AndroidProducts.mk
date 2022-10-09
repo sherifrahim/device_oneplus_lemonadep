c42d6a7b37403db9874887dbdf473b81d2cf7b28
Sherif Rahim <28657313+sherifrahim@users.noreply.github.com>
2022-10-09 19:57:14 +0530

    xd

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lemonadep.mk
+    $(LOCAL_DIR)/xdroid_lemonadep.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lemonadep-user \
-    lineage_lemonadep-userdebug \
-    lineage_lemonadep-eng
+    xdroid_lemonadep-user \
+    xdroid_lemonadep-userdebug \
+    xdroid_lemonadep-eng
