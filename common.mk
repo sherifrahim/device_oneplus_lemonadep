d2108f08a5bf8c0067ed5432b97c46db3cd4e38e
Sherif Rahim <sherifrahim2001@gmail.com>
2022-12-25 23:42:11 +0530

    [SQUASH]:sm8350-common: Switch to source built hwc
    *display:drop libdpps
    *display:build some required packages
    *display:override props
    Co-authored-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- common.mk
+++ common.mk
@@ -135,34 +135,24 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
 # Display
-PRODUCT_PACKAGES += \
-    android.hardware.graphics.common-V1-ndk_platform.vendor \
-    android.hardware.graphics.mapper@3.0-impl-qti-display \
-    android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    libdisplayconfig.qti \
-    libdisplayconfig.system.qti \
-    libmemutils \
-    libqdMetaData \
-    libqdMetaData.system \
-    libsdmcore \
-    libsdmutils \
-    libtinyxml \
-    memtrack.default \
-    vendor.display.config@1.0 \
-    vendor.display.config@1.15.vendor \
-    vendor.display.config@2.0 \
-    vendor.display.config@2.0.vendor \
-    vendor.qti.hardware.display.allocator-service \
-    vendor.qti.hardware.display.composer-service \
-    vendor.qti.hardware.display.mapper@1.1.vendor \
-    vendor.qti.hardware.display.mapper@2.0.vendor \
-    vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.common-V1-ndk.vendor \
+    libqdutils \
+    libqservice \
+    libtinyxml
+
+PRODUCT_VENDOR_PROPERTIES += \
+    debug.sf.frame_rate_multiple_threshold=60 \
+    ro.surface_flinger.refresh_rate_switching=true \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true \
+    vendor.display.primary_mixer_stages=9 \
+    vendor.display.enable_async_powermode=0
+
+$(call inherit-product, hardware/qcom-caf/sm8350/display/config/display-product.mk)
 
 # Doze
 PRODUCT_PACKAGES += \
